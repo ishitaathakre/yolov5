1c06b5eaf2507270b8027ac3ef41c3bfef1b1de1
ishitaathakre <ishitathakre1@gmail.com>
2024-04-27 21:55:22 +0530

    removed label

--- detect.py
+++ detect.py
@@ -211,7 +211,7 @@ def run(
 
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                        line = (*xywh, conf) if save_conf else (cls, *xywh)  # label format
                         with open(f"{txt_path}.txt", "a") as f:
                             f.write(("%g " * len(line)).rstrip() % line + "\n")
 
